3ff6861ca7bcd36be677f4434a24d9eca7dcb683
Stephanie <sbowl001@fiu.edu>
2020-06-18 10:31:10 +0800

    project created

---
+++ Game of Life/Game of Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game of Life
+//
+//  Created by Stephanie Bowles on 6/18/20.
+//  Copyright © 2020 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
